9598c3a3ddffd2dc304f941df727ae5a60bdc1f8
Platos-Man <joonasru@tutanota.com>
2024-04-30 16:59:09 +0300

    created basic view for Product model

--- backend/online_store/urls.py
+++ backend/online_store/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products.views import ProductListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductListView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
